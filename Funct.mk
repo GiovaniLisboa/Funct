02b9ddf2664bee8d5e8e9eb8db2a394708ef8a28
Giovani Lisboa <giovanilisboa@hotmail.com>
2017-11-01 19:39:22 -0300

    Atividade 01.11.2017

--- Funct.mk
+++ Funct.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=Funct
 ConfigurationName      :=Debug
-WorkspacePath          :="C:/Users/giova/Documents/Projetos C++/PrimeiroProjeto"
-ProjectPath            :="C:/Users/giova/Documents/Projetos C++/PrimeiroProjeto/Funct"
+WorkspacePath          :=C:/Users/201711140029/Documents/PrimeiroProjeto
+ProjectPath            :=C:/Users/201711140029/Documents/PrimeiroProjeto/Funct
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=giova
-Date                   :=31/10/2017
+User                   :=201711140029
+Date                   :=01/11/2017
 CodeLitePath           :="C:/Program Files/CodeLite"
-LinkerName             :=C:/TDM-GCC-64/bin/g++.exe
-SharedObjectLinkerName :=C:/TDM-GCC-64/bin/g++.exe -shared -fPIC
+LinkerName             :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe"
+SharedObjectLinkerName :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe" -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -36,7 +36,7 @@ ObjectsFileList        :="Funct.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 RcCmpOptions           := 
-RcCompilerName         :=C:/TDM-GCC-64/bin/windres.exe
+RcCompilerName         :="C:/Program Files (x86)/CodeBlocks/MinGW/bin/windres.exe"
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -49,13 +49,13 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := C:/TDM-GCC-64/bin/ar.exe rcu
-CXX      := C:/TDM-GCC-64/bin/g++.exe
-CC       := C:/TDM-GCC-64/bin/gcc.exe
+AR       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/ar.exe" rcu
+CXX      := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/g++.exe"
+CC       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc.exe"
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/TDM-GCC-64/bin/as.exe
+AS       := "C:/Program Files (x86)/CodeBlocks/MinGW/bin/as.exe"
 
 
 ##
@@ -94,12 +94,12 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/giova/Documents/Projetos C++/PrimeiroProjeto/Funct/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/201711140029/Documents/PrimeiroProjeto/Funct/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
 
 $(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix)main.cpp
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
--- main.cpp
+++ main.cpp
@@ -4,24 +4,54 @@ using std::cout;
 using std::cin;
 
 int cubo(int);
+double cubo(double);
+void cuboRef(int &);
+
+int a=10;
 
 int main(int argc, char **argv)
 {
-    int a;
-    int resultado;
+    int a, resultadoA;
+    double b, resultadoB;
     
-    cout << "Entre com o valor a ser calculado: ";
+    cout << "Entre com o valor inteiro A a ser calculado: ";
     cin >> a;
     cout << "\n";
     
-    resultado = cubo(a);
+    resultadoA = cubo(a);
+    cout << "Resultado do cubo de A: " << resultadoA << "\n";
+    cout << "A: " << a << "\n \n";
     
-    cout << a << " elevado ao cubo: " << resultado << "\n";
+    cuboRef(a);
+    cout << "Resultado do cubo de A: " << a << "\n";
+    cout << "A: " << a << "\n \n";
+    
+    resultadoA = cubo(::a);
+    cout << "Resultado do cubo de ::A: " << resultadoA << "\n";
+    cout << "::A: " << ::a << "\n \n";
+    
+    cout << "Entre com o valor fracionado B a ser calculado: ";
+    cin >> b;
+    cout << "\n";
+    
+    resultadoB = cubo(b);
+    cout << "Resultado do cubo de B: " << resultadoB << "\n";
+    cout << "B: " << b << "\n \n";
     
     return 0;
 }
 
 int cubo (int x)
 {
-    return x*x*x;
+    return x * x * x;
+}
+
+double cubo (double x)
+{
+    return x * x * x;
+}
+
+void cuboRef (int &x)
+{
+    x *= x * x;
 }
